5b476d43667bbde5226625715c27188cebda8265
NHclub <sbe4488@naver.com>
2023-09-17 20:09:29 +0900

    [Bronze I] Title: 단어 공부, Time: 280 ms, Memory: 21060 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String word = bf.readLine().toUpperCase();
+        int[] alphabet = new int[26];
+        int max = 0;
+        char result = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            alphabet[word.charAt(i) - 'A'] += 1;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (alphabet[i] > max) {
+                max = alphabet[i];
+                result = (char)(i+'A');
+            } else if (alphabet[i] == max) {
+                result = '?';
+            }
+        }
+
+        System.out.println(result);
+    }
+
+}
