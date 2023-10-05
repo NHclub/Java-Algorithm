f26a4f3ad0b310fbabda0cc0ebd4fc7503a7c985
NHclub <sbe4488@naver.com>
2023-10-05 20:08:29 +0900

    [level 2] Title: 올바른 괄호, Time: 20.58 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,18 +1,14 @@
 import java.util.*;
 class Solution {
-    boolean solution(String s) {
+        boolean solution(String s) {
         boolean answer = true;
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (ch == '(') {
-                stack.push(s.charAt(i));
-            } else if (ch == ')' && stack.isEmpty()) {
-                return answer = false;
-            } else if (ch == ')' && !stack.isEmpty()) {
-                stack.pop();
-            }
+            if (ch == '(') stack.push(s.charAt(i));
+            else if (ch == ')' && stack.isEmpty()) return answer = false;
+            else if (ch == ')' && !stack.isEmpty()) stack.pop();
         }
 
         answer = stack.isEmpty() ? true : false;
